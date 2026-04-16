b9afff8a34aae0688c6ed48a3b1cfce9c3fda475
Minko82 <obesecoconut@protonmail.com>
2026-04-15 23:12:27 -0600

    fix: improving connectivity

--- legacy_cap_2/legacy_cap_2.ino
+++ legacy_cap_2/legacy_cap_2.ino
@@ -1,7 +1,7 @@
 #include "CapacitiveSensorR4.h" // to use Arduino Uno R3, change this to Paul Bagder's Capacitive Sensing Library
 
 const int SEND_PIN = 19;
-const int SAMPLES = 30;
+const int SAMPLES = 30; 
 
 CapacitiveSensor cs_1 = CapacitiveSensor(SEND_PIN, 23);
 CapacitiveSensor cs_2 = CapacitiveSensor(SEND_PIN, 22);
@@ -14,26 +14,27 @@ CapacitiveSensor cs_7 = CapacitiveSensor(SEND_PIN, 4);
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Arduino has started!");
 }
 
 void loop() {
    //resistor should be pin 5 (start => in node)
   long sensorValue1 = cs_1.capacitiveSensor(SAMPLES);
-  // long sensorValue2 = cs_2.capacitiveSensor(SAMPLES);
+  long sensorValue2 = cs_2.capacitiveSensor(SAMPLES);
   long sensorValue3 = cs_3.capacitiveSensor(SAMPLES);
-  // long sensorValue4 = cs_4.capacitiveSensor(SAMPLES);
+  long sensorValue4 = cs_4.capacitiveSensor(SAMPLES);
   long sensorValue5 = cs_5.capacitiveSensor(SAMPLES);
-  // long sensorValue6 = cs_6.capacitiveSensor(SAMPLES);
-  // long sensorValue7 = cs_7.capacitiveSensor(SAMPLES);
+  long sensorValue6 = cs_6.capacitiveSensor(SAMPLES);
+  long sensorValue7 = cs_7.capacitiveSensor(SAMPLES);
   
   //Serial.print("S6,");
   Serial.print(sensorValue1); Serial.print(",");
-  // Serial.print(sensorValue2); Serial.print(",");
+  Serial.print(sensorValue2); Serial.print(",");
   Serial.print(sensorValue3); Serial.print(",");
-  // Serial.print(sensorValue4); Serial.print(",");
-  // Serial.print(sensorValue5); Serial.print(",");
-  // Serial.print(sensorValue6); Serial.print(",");
-  Serial.println(sensorValue5);
+  Serial.print(sensorValue4); Serial.print(",");
+  Serial.print(sensorValue5); Serial.print(",");
+  Serial.print(sensorValue6); Serial.print(",");
+  Serial.println(sensorValue7);
 } 
 
 
